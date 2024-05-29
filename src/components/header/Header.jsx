2ef60a0f0d9c490f957ed707cc2201743a37f994
Yane Ully <yaneully@gmail.com>
2024-05-29 18:20:05 +0100

    created the navbar component and the items in the navbar

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,60 @@
+import './header.css';
+
+function Header() {
+  return (
+    <header className='header'>
+      <nav className='nav container'>
+        <a href='index.html' className='nav__logo'>
+          Yane Ully
+        </a>
+        <div className='nav__menu'>
+          <ul className='nav__list grid'>
+            <li className='nav__item'>
+              <a href='#home' className='nav__link active-link'>
+                <i className='uil uil-estate nav__icon'></i> Home
+              </a>
+            </li>
+
+            <li>
+              <a href='#about' className='nav__link'>
+                <i className='uil uil-user nav__icon'></i> About
+              </a>
+            </li>
+
+            <li>
+              <a href='#skills' className='nav__link'>
+                <i className='uil uil-file nav__icon'></i> Skills
+              </a>
+            </li>
+
+            <li>
+              <a href='#services' className='nav__link'>
+                <i className='uil uil-briefcase-alt nav__icon'></i> Services
+              </a>
+            </li>
+
+            <li>
+              <a href='portfolio' className='nav__link'>
+                <i className='uil uil-scenery nav__icon'></i> Portfolio
+              </a>
+            </li>
+
+            <li>
+              <a href='#contact' className='nav__link'>
+                <i className='uil uil-message nav__icon'></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i className='uil uil-times nav__close'></i>
+        </div>
+
+        <div className='nav__toggle'>
+          <i className='uil uil-apps'></i>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
