22f5139ecd1b8af306988c80faffa1ac1f159f27
Yane Ully <yaneully@gmail.com>
2024-05-30 11:35:44 +0100

    fixing the navbar icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Navbar from './scenes/Navbar';
+import { useState } from 'react';
+import useMediaQuery from './hooks/useMediaQuery';
 import './App.css';
-import Header from './components/header/Header';
 
 function App() {
+  // where in navigation we are
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+
   return (
-    <>
-      <Header />
-    </>
+    <div className='app bg-light-green'>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
   );
 }
 
