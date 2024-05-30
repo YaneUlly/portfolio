8f56576723594b5378c6e996422eba88139cdfc7
Yane Ully <yaneully@gmail.com>
2024-05-30 14:52:21 +0100

    finish the navbar

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -16,12 +16,13 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   );
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery('(min-width: 768px)');
+  const navbarBackground = isTopOfPage ? '' : 'bg-red';
 
   return (
-    <nav className={`z-40 w-full fixed top-0 py-6`}>
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className='flex items-center justify-between mx-auto w-5/6'>
         <h4 className='font-poppins text-3xl font-bold'>YU</h4>
 
@@ -62,16 +63,16 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
 
         {/* MOBILE MENU POPUP  */}
         {!isAboveSmallScreens && isMenuToggled && (
-          <div className='fixed right-0 bottom-0 h-full bg-blue w-[300px]'>
+          <div className='fixed right-0 bottom-0 h-full bg-black w-[300px]'>
             {/* CLOSE ICON */}
             <div className='flex justify-end p-12'>
               <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                <span className='material-icons'>close</span>
+                <span className='material-icons text-white'>close</span>
               </button>
             </div>
 
             {/* MENU ITEMS  */}
-            <div className='flex flex-col gap-10 ml-[33%] text-2xl'>
+            <div className='flex flex-col gap-10 ml-[33%] text-2xl text-white'>
               <Link
                 page='Home'
                 selectedPage={selectedPage}
