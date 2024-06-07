0989d9be04c545dd6622f75f97044fabdb52e23c
Yane Ully <yaneully@gmail.com>
2024-06-07 15:33:06 +0100

    fill the first part of the landing page

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const SocialMediaIcons = () => {
+  return (
+    <div className='flex justify-center md:justify-start my-10 gap-7'>
+      <a
+        className='hover:opacity-50 transition duration-500'
+        href='https://www.linkedin.com/in/yane-ully-martins/'
+        target='_blank'
+        rel='noreferrer'
+      >
+        linkedin
+      </a>
+
+      <a
+        className='hover:opacity-50 transition duration-500'
+        href='https://github.com/YaneUlly'
+        target='_blank'
+        rel='noreferrer'
+      >
+        GitHub
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,5 +1,5 @@
+import SocialMediaIcons from '../components/SocialMediaIcons';
 import useMediaQuery from '../hooks/useMediaQuery';
-import profile from '../assets/profile_portfolio.jpeg';
 import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import '../App.css';
@@ -17,8 +17,69 @@ const Landing = ({ setSelectedPage }) => {
         {isAboveMediumScreens ? (
           <div className='profile__img'></div>
         ) : (
-          <div></div>
+          <div className='profile__img'></div>
         )}
+      </div>
+      {/* MAIN SECTION  */}
+      <div className='z-30 basis-2/5 mt-12 md:mt-32'>
+        {/* SOCIAL MEDIA ICONS  */}
+        <motion.div
+          className='flex mt-5 justify-center md:justify-start'
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
+
+        {/* HEADINGS */}
+        <motion.div
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className='text-6xl font-poppins z-10 text-center md:text-start'>
+            Yane Ully
+          </p>
+          <p className='mt-10 mb-7 text-sm text-center md:text-start'>
+            UI Developer & Product Designer
+          </p>
+          <p className='mt-10 mb-7 text-sm text-center md:text-start'>
+            Im creative UI developer based in Lisbon, and Im very passionate and
+            dedicated to my work
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTION  */}
+        <motion.div
+          className='flex mt-5 justify-center md:justify-start'
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className='bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500 '
+            onClick={() => setSelectedPage('contact')}
+            href='#contact'
+          >
+            Say hello
+          </AnchorLink>
+        </motion.div>
       </div>
     </section>
   );
